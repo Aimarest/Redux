15c8ba34274eb617e6875155324a08d19990792d
Ana <amarcosestevez@gmail.com>
2022-09-28 11:35:54 +0200

    add first task redux solved

--- toolkit-redux/src/App.jsx
+++ toolkit-redux/src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
+import { increment, decrement, incrementBy } from './store/slices/counter';
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+const { counter } = useSelector( state => state.counter )
+const dispatch = useDispatch();
 
   return (
     <div className="App">
       <header className='App-header'>
         <img src="/vite.svg" className="logo" alt="Vite logo" />
-        <p>Vite + React</p>
+        <p>count is { counter }</p>
         <p className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
+          <button onClick={() => dispatch( increment() )}>
+           increment
+          </button>
+          <button onClick={() => dispatch( decrement() )}>
+           decrement
+          </button>
+          <button onClick={() => dispatch( incrementBy(2) )}>
+           Increment by 2
           </button>
         </p>
       </header>
