04bbea8b35a17152bc533ab46e1409dddef8a208
Ana <amarcosestevez@gmail.com>
2022-09-28 14:06:11 +0200

    setPokemons sin axios

--- toolkit-redux/src/store/slices/pokemon/thunks.js
+++ toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -1,4 +1,4 @@
-import { startLoadingPokemons } from "./pokemonSlice"
+import { startLoadingPokemons, setPokemons } from "./pokemonSlice"
 
 
 
@@ -13,6 +13,6 @@ export const getPokemons = (page = 0) => {
        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`)
         const data = await resp.json();
         console.log(data)
-        // dispatch (setPokemons());
+         dispatch (setPokemons({ pokemons: data.results, page: page + 1}));
     }
 }
